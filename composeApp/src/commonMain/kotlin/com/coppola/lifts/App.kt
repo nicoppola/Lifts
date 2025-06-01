3d716cf6b210f17b8b1106834c2596b1f2a31832
coppolan <coppolan32@gmail.com>
2025-06-01 15:28:24 -0500

    basic dark light theming

--- composeApp/src/commonMain/kotlin/com/coppola/lifts/App.kt
+++ composeApp/src/commonMain/kotlin/com/coppola/lifts/App.kt
@@ -1,18 +1,18 @@
 package com.coppola.lifts
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import com.coppola.lifts.main.MainScreen
-import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.compose.KoinContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.coppola.lifts.main.MainScreen
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.KoinContext
+import com.coppola.lifts.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    AppTheme {
         KoinContext{
             NavHost(
                 navController = rememberNavController(),
---
+++ composeApp/src/commonMain/kotlin/com/coppola/lifts/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.coppola.lifts.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
